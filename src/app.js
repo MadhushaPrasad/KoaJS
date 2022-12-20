a11af4dc10fb0e5e3ac49e70117ccf3ed4bfcb7f
Madhusha Prasad <50085447+MadhushaPrasad@users.noreply.github.com>
2022-12-20 11:31:14 +0530

    add prefix for index routes

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 const koa = require('koa')
 const app = new koa()
 const router = require('./routes/index.router')
-
 app.use(router.routes()).use(router.allowedMethods());
+
+
 
 app.listen(8000, () => {
     console.log('Server running at PORT 8000')
--- src/routes/index.router.js
+++ src/routes/index.router.js
@@ -2,9 +2,28 @@ const KoaRouter = require('koa-router');
 
 const router = new KoaRouter();
 
+router.prefix("/koaFund")
+
 router.get('/', async (ctx) => {
     ctx.body = 'Hello World';
 });
+
+router.post('/create', async (ctx) => {
+    ctx.body = 'This is post API';
+});
+
+router.put('/create', async (ctx) => {
+    ctx.body = 'This is put API';
+});
+
+router.delete('/create', async (ctx) => {
+    ctx.body = 'This is delete API';
+});
+
+router.get('/:id', async (ctx) => {
+    ctx.body = `This is search API and ID is ${ctx.request.body.id}`;
+});
+
 
 module.exports = router;
 
