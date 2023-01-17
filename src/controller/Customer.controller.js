87f7df178b72cd848f1310326649f6524c0f6f42
MadhushaPrasad <madushaprasad21@gmail.com>
2023-01-17 09:28:23 +0530

    make customer search filter function

--- src/controller/Customer.controller.js
+++ src/controller/Customer.controller.js
@@ -1,57 +1,88 @@
 const saveCustomer = async (req, res) => {
-    console.log("saveCustomer",req.body);
-    try {
-        res.status = 200;
-        res.body = req.body;
-    } catch (e) {
-        console.log(e);
-    }
-}
+  console.log("saveCustomer", req.body);
+  try {
+    res.status = 200;
+    res.body = req.body;
+  } catch (e) {
+    console.log(e);
+  }
+};
 
 const updateCustomer = async (req, res) => {
-    console.log("updateCustomer");
-    try {
-        res.status = 200;
-        res.body = "Customer updated successfully";
-    } catch (e) {
-        console.log(e);
-    }
-}
+  console.log("updateCustomer");
+  try {
+    res.status = 200;
+    res.body = req.body;
+  } catch (e) {
+    console.log(e);
+  }
+};
 
 const deleteCustomer = async (req, res) => {
-    console.log("deleteCustomer");
-    try {
-        res.status = 200;
-        res.body = "Customer deleted successfully";
-    } catch (e) {
-        console.log(e);
-    }
-}
+  console.log("deleteCustomer");
+  try {
+    res.status = 200;
+    res.body = req.body;
+  } catch (e) {
+    console.log(e);
+  }
+};
 
 const searchCustomer = async (req, res) => {
-    console.log("searchCustomer");
-    try {
+  console.log("searchCustomer");
+  const customerList = {
+    customers: [
+      {
+        id: 1,
+        name: "John",
+        age: 25,
+      },
+      {
+        id: 2,
+        name: "Peter",
+        age: 30,
+      },
+    ],
+  };
+  try {
+    customerList.customers.forEach((customer) => {
+      if (customer.id == req.params.id) {
         res.status = 200;
-        res.body = "Customer searched successfully and ID is " + req.params.id;
-    } catch (e) {
-        console.log(e);
-    }
-}
+        res.body = customer;
+      }
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
 
 const getAllCustomers = async (req, res) => {
-    console.log("getAllCustomers");
-    try {
-        res.status = 200;
-        res.body = "All customers";
-    } catch (e) {
-        console.log(e);
-    }
-}
+  console.log("getAllCustomers");
+  try {
+    res.status = 200;
+    res.body = {
+      customers: [
+        {
+          id: 1,
+          name: "John",
+          age: 25,
+        },
+        {
+          id: 2,
+          name: "Peter",
+          age: 30,
+        },
+      ],
+    };
+  } catch (e) {
+    console.log(e);
+  }
+};
 
 module.exports = {
-    saveCustomer,
-    updateCustomer,
-    deleteCustomer,
-    searchCustomer,
-    getAllCustomers
-}
+  saveCustomer,
+  updateCustomer,
+  deleteCustomer,
+  searchCustomer,
+  getAllCustomers,
+};
